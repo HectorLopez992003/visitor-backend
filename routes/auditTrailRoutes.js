d878566f28313ca07e2be0d6b93f024272697fd7
Hector Lopez <hectorjoshlopez@gmail.com>
2026-02-08 13:13:32 +0800

    describe what you changed

--- routes/auditTrailRoutes.js
+++ routes/auditTrailRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import AuditTrail from "../models/AuditTrail.js";
-import { verifyJWT } from "../middleware/auth.js";
+import { verifyJWT, requireRole } from "../middleware/auth.js";
 
 const router = express.Router();
 
